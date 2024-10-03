f77a4c9f2b408dbf9318b5856501682bff407995
Jon Caveman <jon@joncaveman.com>
2024-10-03 14:31:52 -0400

    new react site

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section className="hero highlighted-text-shadow">
+      <p>
+        We are<br />
+        A Creative<br />
+        Technology
+        <br />Company
+      </p>
+      {/* <Spline scene="https://prod.spline.design/JH5waopj4xDj1vad/scene.splinecode" /> */}
+    </section>
+  );
+}
+
+export default Hero;
