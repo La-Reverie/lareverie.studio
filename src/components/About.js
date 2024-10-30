62681e0a11f38f68093c6bffc9a19dd2a30ea04b
Jon Caveman <jon@joncaveman.com>
2024-10-30 18:05:41 -0400

    updated about us section with bio from thesis

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,12 @@ function About() {
         </h2>
 
         <article className="text-black text-base font-normal leading-normal pb-3 pt-1">
-          <span className='standout-01'>Make ideas come true</span>. We are a creative technology company. But even more, a passionate group of doers, awakening our inner child's and manifesting the kind of world in which we want to live.
-          <br /><br />
-          <span className='standout-01'>Our clients are folks that we believe in.</span> That believe in us. Your success is our success. You're cordially invited.
+          <p className='mb-5'>
+            <span className='company'>La Reverie Studio</span> draws its spirit from <i>The Little Prince</i>, inviting us to see the world through the lens of childhood wonder—a time when everything was magical, creativity flowed effortlessly, and we were fully present without the need for explanations. At <span className='company'>La Reverie Studio</span>, we believe in tapping into that boundless curiosity, where each project feels like a fresh discovery and every design emerges from an open heart and imaginative mind. This approach isn’t just nostalgic; it’s a journey back to our truest selves, unburdened by convention or expectation.
+          </p>
+          <p>
+            Our studio embraces this ethos by creating digital experiences that are authentic, playful, and deeply resonant. Whether it’s crafting a platform or designing a brand identity, we aim to bring a touch of innocence and wonder to our work, celebrating creativity that feels pure, unfiltered, and genuinely engaging. Like <i>The Little Prince</i>, who sees the beauty and mystery in even the simplest things, we approach each project with a sense of hope and wide-eyed curiosity, allowing our clients’ visions to shine in their most meaningful form.
+          </p>
         </article>
       </div>
     </section>
