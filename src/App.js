4eb98ffe25a22faf8bc3e77700849dd11361717c
Javier Cabrera <doncabreraphone@gmail.com>
2025-01-25 01:19:32 -0300

    Loading Working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Frame from './components/Frame';
 import Hero from './components/Hero';
 // import Pseudo3DBackground from './components/Pseudo3DBackground';
 import Home from './components/Home';
+import DynamicSceneLoader from './components/DynamicSceneLoader';
 import Sales from './components/Sales';
 import TheStudio from './components/TheStudio';
 import Team from './components/Team';
@@ -14,9 +15,11 @@ import './global.css';
 import './App.css';
 
 function App() {
-    
     return (
         <div className="App">
+            <div className="w-full h-screen">
+                <DynamicSceneLoader />
+            </div>
             {/* <Pseudo3DBackground 
                 image="/img/woman.jpeg"
                 depthMap="/img/woman.png"
